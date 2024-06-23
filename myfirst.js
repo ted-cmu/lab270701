b35ee6733cab8e34d2119b400d0693ef60366015
ted-cmu <htet_a@cmu.ac.th>
2024-06-23 04:06:35 +0000

    second commit

--- myfirst.js
+++ myfirst.js
@@ -1,6 +1,23 @@
-let http = require('http');
+// let http = require('http');
 
-http.createServer(function(req,res){
-  res.writeHead(200,{'Content-Type':'text/html'});
+// http.createServer(function(req, res){
+//   res.writeHead(200, {'Content-Type': 'text/html'});
+//   res.end('Hello World');
+// }).listen(3000, '127.0.0.1');
+
+
+
+const { createServer } = require('node:http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
   res.end('Hello World');
-}).listen(9000);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
